493b962032f9c8212243b2a4c95f5f1695d19d5c
Manuel Romero <manu8083el@gmail.com>
2025-01-28 16:17:45 -0500

    Add hook

--- react/ProductContext.tsx
+++ react/ProductContext.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { useProduct } from 'vtex.product-context'
+import { useRuntime } from 'vtex.render-runtime'
+import { useOrderForm } from 'vtex.order-manager/OrderForm';
 
-
-function ProductContext() {
+function ProductContext() {/*  */
   const productContextValue = useProduct();
+  const runtime = useRuntime()
+  const { orderForm } = useOrderForm();
+
+  console.log("useRuntime", runtime)
+  console.log("orderForm", orderForm)
   console.log("productContextValue", productContextValue)
+
+
   return <div>Nombre del producto:, {productContextValue?.product?.productName} </div>
 }
 
