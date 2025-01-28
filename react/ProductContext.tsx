a0c01bd5dfea382cf570f6a22a95ad3afc881f14
Manuel Romero <manu8083el@gmail.com>
2025-01-28 17:12:09 -0500

    Change info

--- react/ProductContext.tsx
+++ react/ProductContext.tsx
@@ -1,19 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useProduct } from 'vtex.product-context'
 import { useRuntime } from 'vtex.render-runtime'
-import { useOrderForm } from 'vtex.order-manager/OrderForm';
+import { useOrderForm } from 'vtex.order-manager/OrderForm'
+import { useQuery } from 'react-apollo'
 
-function ProductContext() {/*  */
-  const productContextValue = useProduct();
+import GET_APP_SETTINGS from '../queries/getAppSetting.graphql'
+
+function ProductContext() {
+  const productContextValue = useProduct()
   const runtime = useRuntime()
-  const { orderForm } = useOrderForm();
+  const { orderForm } = useOrderForm()
 
-  console.log("useRuntime", runtime)
-  console.log("orderForm", orderForm)
-  console.log("productContextValue", productContextValue)
+  const { data, loading, error } = useQuery(GET_APP_SETTINGS, {
+    variables: { app: `qamkpmovilidadco.frontend-app` },
+  })
 
+  const appSettings = (() => {
+    try {
+      const rawMessage = data?.appSettings?.message
 
-  return <div>Nombre del producto:, {productContextValue?.product?.productName} </div>
+      return JSON.parse(rawMessage || '{}')
+    } catch {
+      return {}
+    }
+  })()
+
+  useEffect(() => {
+    console.info('Procesado appSettings:', appSettings)
+  }, [appSettings])
+
+  console.info('productContextValue', productContextValue)
+  console.info('runtime', runtime)
+  console.info('orderForm', orderForm)
+
+  return (
+    <div>
+      <h1>ProductContext</h1>
+      {loading && <p>Cargando datos...</p>}
+      {error && <p>Error al cargar datos: {error.message}</p>}
+      {!loading && !error && (
+        <>
+          <h2>Configuración de la Aplicación:</h2>
+          <pre>{JSON.stringify(appSettings, null, 2)}</pre>
+        </>
+      )}
+    </div>
+  )
 }
 
 export default ProductContext
